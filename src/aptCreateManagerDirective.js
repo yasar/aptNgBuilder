6617bc4f80179bf219d348870dfd0621b043d9b3
Yasar Bayar <yasarbayar@gmail.com>
2016-08-22 13:01:29 +0300

    ..

---
+++ src/aptCreateManagerDirective.js
@@ -0,0 +1,101 @@
+/**
+ * Created by yasar on 12.05.2016.
+ */
+
+
+function aptCreateManagerDirective(builder) {
+
+    var suffix = builder.suffix.manager;
+
+    angular
+        .module(builder.getModuleName())
+        .directive(builder.getDirectiveName(suffix), DirectiveFactory);
+
+    DirectiveFactory.$inject = ['$injector'];
+    function DirectiveFactory($injector) {
+        return new Directive(builder, $injector);
+    }
+
+    function Directive(builder, $injector) {
+        aptCreateManagerDirective.ctr++;
+
+        var directive = {
+            restrict        : 'EA', // ACME
+            scope           : {
+                /**
+                 * different modules may require different things.
+                 * that's why we are accepting any of the followings.
+                 * it is the manager's responsibility to handle them.
+                 * manager is expected to be defined in the module file.
+                 */
+                item  : '=?',
+                items : '=?',
+                itemId: '<?',
+                /**
+                 * for passing-in extra params
+                 * this is usually best practiced with an object.
+                 */
+                params: '<?'
+            },
+            templateUrl     : templateUrlFn,
+            link            : linkFn,
+            controller      : Controller,
+            controllerAs    : builder.getControllerAsName(suffix),
+            bindToController: true
+        };
+
+        if (_.has(builder, 'form.require')) {
+            _.set(directive, 'require', _.get(builder, 'form.require'));
+        }
+
+        return directive;
+
+        function templateUrlFn(elem, attrs) {
+            /**
+             * Check the item attribute.
+             * If we do have an item available, that means we are in edit mode, and should use update-form
+             * if there is no item, then we are in new mode, and should use add-form
+             */
+            if (attrs.viewType) {
+                return builder.getPath(suffix) + '/' + suffix + '-' + attrs.viewType + '.tpl.html';
+            } else {
+                return builder.getPath(suffix) + '/' + suffix + '.tpl.html';
+            }
+        }
+
+        function linkFn(scope, elem, attrs, ctrls, x) {
+            if (builder.manager && builder.manager.link && angular.isFunction(builder.manager.link)) {
+                builder.manager.link.call(this, $injector, builder, scope, elem, attrs, ctrls);
+            }
+        }
+    }
+
+    Controller.$inject = ['$injector', '$scope'];
+    function Controller($injector, $scope) {
+        var vm       = this;
+        var service  = $injector.get(builder.getServiceName('Service'));
+        var aptUtils = $injector.get('aptUtils');
+
+        if (builder.manager && builder.manager.beforeDataLoad && angular.isFunction(builder.manager.beforeDataLoad)) {
+            builder.manager.beforeDataLoad.call(this, $injector, $scope, builder);
+        }
+
+        // if (_.isUndefined(vm.item) && _.isUndefined(vm.items) && !_.isUndefined(vm.itemId)) {
+        if (!vm.item && !vm.items && vm.itemId) {
+            vm.item = {};
+            service.get(vm.itemId).then(function (data) {
+                //aptUtils.removeAndMerge(vm.item, data);
+                _.merge(vm.item, data);
+                if (_.isFunction(_.get(builder, 'manager.onDataLoad'))) {
+                    builder.manager.onDataLoad();
+                }
+            });
+        }
+
+        if (builder.manager && builder.manager.controller && angular.isFunction(builder.manager.controller)) {
+            builder.manager.controller.call(this, $injector, $scope, builder);
+        }
+
+    }
+}
+aptCreateManagerDirective.ctr = 0;
---
+++ src/aptCreateModelService.js
@@ -0,0 +1,156 @@
+/**
+ * Created by yasar on 17.01.2016.
+ */
+
+function aptCreateModelService(builder) {
+    {
+        var modelDefaults = {
+            methods: {
+                collection: [{name: 'search', httpMethod: 'getList', route: 'search'}],
+                element   : [{name: 'search', httpMethod: 'getList', route: 'search'}]
+            }
+        };
+
+        if (builder.enableStatusUpdate) {
+            modelDefaults.methods.collection.push({
+                name: 'getApplicableStatuses', httpMethod: 'getList', route: 'getApplicableStatuses'
+            });
+        }
+
+        /**
+         * define default service methods
+         */
+        _.mergeWith(builder.model, modelDefaults, function (objValue, srcValue) {
+            if (_.isArray(objValue)) {
+                return objValue.concat(srcValue);
+            }
+        });
+    }
+
+
+    angular
+        .module(builder.getModuleName())
+        .factory(builder.getServiceName('model'), factoryFn);
+
+    factoryFn.$inject = ['$injector'];
+    function factoryFn($injector) {
+        if (!this.srv) {
+            var Restangular = $injector.get('Restangular'),
+                srv         = Restangular
+                    .withConfig(function (RestangularConfigurer) {
+                        RestangularConfigurer.setRestangularFields({
+                            id: builder.getPrimaryKey()
+                        });
+
+                        if (angular.isArray(builder.model.methods.collection)
+                            && builder.model.methods.collection.length > 0) {
+                            RestangularConfigurer.addElementTransformer(builder.getRoute(), true /* collection */,
+                                function (restObject) {
+
+                                    if (!angular.isObject(restObject)) {
+                                        return restObject;
+                                    }
+
+                                    angular.forEach(builder.model.methods.collection, function (method) {
+                                        restObject.addRestangularMethod(method.name, method.httpMethod, method.route);
+                                    });
+
+                                    return restObject;
+                                });
+                        }
+
+                        if (angular.isArray(builder.model.methods.element)
+                            && builder.model.methods.element.length > 0) {
+                            RestangularConfigurer.addElementTransformer(builder.getRoute(), false /* element */,
+                                function (restObject) {
+
+                                    if (!angular.isObject(restObject)) {
+                                        return restObject;
+                                    }
+
+                                    angular.forEach(builder.model.methods.element, function (method) {
+                                        restObject.addRestangularMethod(method.name, method.httpMethod, method.route);
+                                    });
+
+                                    return restObject;
+                                });
+                        }
+
+                        if (angular.isFunction(builder.model.transformer)) {
+                            RestangularConfigurer.addElementTransformer(builder.getRoute(),
+                                function (restObject) {
+
+                                    builder.model.transformer.call(this, restObject, $injector);
+
+                                    return restObject;
+                                });
+                        }
+                    })
+                    .service(builder.getRoute());
+            srv._route      = builder.getRoute();
+            this.srv        = srv;
+        }
+        return this.srv;
+    }
+
+    if (builder.model.requestInterceptors || builder.model.responseInterceptors) {
+        angular
+            .module(builder.getModuleName())
+            .config(configFn);
+
+
+        configFn.$inject = ['$injector'];
+        function configFn($injector) {
+            var RestangularProvider = $injector.get('RestangularProvider');
+
+            if (builder.model.responseInterceptors) {
+                RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+                    var routeUrl = RestangularProvider.configuration.baseUrl + '/' + builder.getRoute();
+                    // if (data && url.indexOf(routeUrl) == 0) {
+                    // if (data && routeUrl == url) {
+                    if (data && (routeUrl == url || url.indexOf(routeUrl + '/') == 0)) {
+                        processInterceptor.call(this, builder.model.responseInterceptors, operation, data, what, url, $injector);
+                    }
+
+                    return data;
+                });
+            }
+            if (builder.model.requestInterceptors) {
+                RestangularProvider.addRequestInterceptor(function (data, operation, what, url, response, deferred) {
+                    var routeUrl = RestangularProvider.configuration.baseUrl + '/' + builder.getRoute();
+                    // if (data && url.indexOf(routeUrl) == 0) {
+                    // if (data && routeUrl == url) {
+                    if (data && (routeUrl == url || url.indexOf(routeUrl + '/') == 0)) {
+                        processInterceptor.call(this, builder.model.requestInterceptors, operation, data, what, url, $injector);
+                    }
+
+                    return data;
+                });
+            }
+        }
+
+        function processInterceptor(interceptors, operation, data, what, url, $injector) {
+            if (_.isArray(interceptors)) {
+                angular.forEach(interceptors, _process);
+            } else {
+                _process(interceptors);
+            }
+
+            function _process(interceptor) {
+                if (interceptor.operation.indexOf(operation) == -1) {
+                    return;
+                }
+
+                if (angular.isArray(data)) {
+                    angular.forEach(data, function (item) {
+                        interceptor.callback.call(this, item, what, url, $injector);
+                    });
+                } else {
+                    interceptor.callback.call(this, data, what, url, $injector);
+                }
+            }
+        }
+    }
+
+
+}
